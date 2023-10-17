ad9f5525861a0689fc7c09469b27d77975a005b2
Prince Yadav <prinsu9415@gmail.com>
2023-10-17 09:18:34 +0530

    first commit

---
+++ src/components/Content.jsx
@@ -0,0 +1,25 @@
+const Content = () => {
+    return (
+        <main>
+            <div className="left">
+                <h1>YOUR FEET DESERVE THE BEST</h1>
+                <p className="slogan">WE'RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST SHOES</p>
+                <div className="btn">
+                    <button className="first">Shop Now</button>
+                    <button className="second">Category</button>
+                </div>
+                <p>AVAILABLE ON</p>
+                <div className="icon">
+                    <img src="/images/Flipkart.png"></img>
+                    <img src="/images/amazon.webp"></img>
+                    <img src="/images/Myntra.png"></img>
+                </div>
+            </div>
+            <div className="right">
+                <img src="/images/shoes.jpg"></img>
+            </div>
+
+        </main>
+    )
+};
+export default Content;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+const Navbar = () =>{
+    return (
+        <nav>
+        <div className="logo">
+            <img src="/images/logo.png"></img>
+        </div>
+        <ul>
+           
+            <li>Home</li>
+            <li>Features</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    
+        <button>Login</button>
+    
+    </nav>
+    )
+};
+export default Navbar;
